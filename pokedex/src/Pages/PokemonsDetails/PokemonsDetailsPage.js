45518f5160c1f754b537ee11bc8a465df42edeae
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-02 15:57:52 -0300

    pokedex2

---
+++ pokedex/src/Pages/PokemonsDetails/PokemonsDetailsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {} from "./styled"
+
+
+
+export const PokemonsDetails = () => {
+   
+   
+        return (
+            <DetailsContainer>
+                <h1>Ver detalhes</h1>
+            </DetailsContainer>
+        );
+    };
+
+
+export default PokemonsDetails
