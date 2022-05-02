e634dc88b176ecfc757e7601cf99cfe3990942d6
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-02 15:04:52 -0300

    pokedex

---
+++ pokedex/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HomePage from './Pages/HomePage/HomePage';
+
+
+function App() {
+
+  
+  return (
+
+     <div>
+     <HomePage/>
+     </div>
+    
+  );
+}
+
+export default App;
+
---
+++ pokedex/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {HomeContainer} from "./styled"
+
+
+
+export const HomePage = () => {
+   
+   
+        return (
+            <HomeContainer>
+                <h1>Pokedex</h1>
+            </HomeContainer>
+        );
+    };
+
+
+export default HomePage
