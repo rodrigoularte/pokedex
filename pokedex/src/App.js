bbdbe8bc7cba856f5d9ebeceb3c445ae6e306119
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-03 15:07:19 -0300

    pokedex3

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -3,6 +3,7 @@ import CardPokemons from './Components/Cards/CardPokemons';
 import HomePage from './Pages/HomePage/HomePage';
 import Pokedex from './Pages/Pokedex/PokedexPage';
 import PokemonsDetails from './Pages/PokemonsDetails/PokemonsDetailsPage';
+import Header from './Components/Header/Header';
 
 
 
@@ -10,13 +11,17 @@ function App() {
 
 
   return (
-
+    
+    
+    
     <div>
+      <Header/>
       <CardPokemons />
       <HomePage />
       <Pokedex />
       <PokemonsDetails />
     </div>
+    
 
   );
 }
