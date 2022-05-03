8d991452f0a559f861ba2fa64799ad42c06e87c6
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-05-03 18:26:41 -0300

    Corrigindo erros

--- pokedex/src/Components/Header/Header.js
+++ pokedex/src/Components/Header/Header.js
@@ -1,36 +1,16 @@
-import React from "react";
-<<<<<<< HEAD
-// import {HeaderContainer} from ""
-import logo from "../../Assets/pokeball.png";
+import React from "react"
+import { HeaderContainer, LogoPokeDev } from "./Styled"
+import pokedexLogo from "../../Assets/pokedex-logo.png"
+
 
 const Header = () => {
+
+
   return (
-    <>
-      {/* <HeaderContainer> */}
-      <img src={logo} alt={"teste"} style={{ width: "30px" }} />
-      {/* </HeaderContainer> */}
-    </>
-  );
-};
-
-export default Header;
-=======
-import { HeaderContainer , LogoPokeDev} from "./Styled";
-import {Pokedex} from "../../Assets/Pokedex.png"
-
-
-const Header = () => {
-    
-
-    return (
-        <HeaderContainer>
-        <img src = {Pokedex}/>
-        </HeaderContainer>
-    )
+    <HeaderContainer>
+      <img src={pokedexLogo} />
+    </HeaderContainer>
+  )
 }
 
-export default Header;
-
-
-
->>>>>>> master
+export default Header
