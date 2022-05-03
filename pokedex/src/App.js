c126091fd6fec37567d1f830eb7cecc6383a6908
michael soto <michael_nickolas@hotmail.com>
2022-05-03 13:14:49 -0300

    Update

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,18 +1,9 @@
-
-// import {HomePage} from './Pages/HomePage/HomePage';
-// import Pokedex from './Pages/Pokedex/PokedexPage';
-// import PokemonsDetails from './Pages/PokemonsDetails/PokemonsDetailsPage';
-
-import {Router} from "./Router/Router";
-
+import { Router } from "./Router/Router";
 
 function App() {
   return (
     <div>
       <Router />
-      {/* <HomePage/>
-     <Pokedex/>
-     <PokemonsDetails/> */}
     </div>
   );
 }
