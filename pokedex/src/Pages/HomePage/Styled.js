14708d391206b8388c007ebbf251707259454165
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-05-03 18:47:52 -0300

    cards adicionados na home

--- pokedex/src/Pages/HomePage/Styled.js
+++ pokedex/src/Pages/HomePage/Styled.js
@@ -2,10 +2,9 @@ import styled from "styled-components"
 
 
 export const HomeContainer = styled.div`
-
-    display: flex;
-    justify-content: center;
-    height: 100vh;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
   flex-wrap: wrap;
-  margin: auto;
+  gap: 30px;
 `
