b6c3a883e0076022a976edd82e9d0325be258482
michael soto <michael_nickolas@hotmail.com>
2022-05-04 18:25:11 -0300

    Update Global,Home e PokeCard

--- src/global/GlobalState.js
+++ src/global/GlobalState.js
@@ -11,45 +11,43 @@ export const GlobalState = (props) => {
   //ANCHOR para página de detalhes do pokemon
   const [pokemonDetails, setPokemonDetails] = useState([]);
 
+  const getPokemons = async () => {
+    //ANCHOR Array estratégico para enviar uma única vez ao state
+    const listPokemon = [];
+
+    //ANCHOR Cria um array de pokemons de acordo com a quantidade de repetições
+    for (let i = 1; i < 21; i++) {
+      await axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+        // .get(`${pokeURL}/${i}/`)
+        .then((response) => {
+          listPokemon[i - 1] = {
+            id: response.data.id,
+            name: response.data.name,
+            status: response.data.stats,
+            moves: response.data.moves,
+            types: response.data.types,
+            sprites: response.data.sprites,
+          };
+          //ANCHOR Se a largura estivar do tamanho que vc desejar,ele vai passar para o state
+          listPokemon.length === 20 && setPokemonList(listPokemon);
+        })
+        .catch((error) => {
+          alert("error.response");
+        });
+    }
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   //ANCHOR variáveis para o provider
   const states = { pokemonList, pokedex, pokemonDetails };
   const setters = { setPokemonList, setPokedex, setPokemonDetails };
+  const requests = { getPokemons };
 
-  const data = { states, setters };
-};
-
-useEffect(() => {
-  getPokemons();
-}, []);
-
-// TODO  Fazendo ainda o GET
-const getPokemons = async () => {
-  //ANCHOR Array estratégico para enviar uma única vez ao state
-  const listPokemon = [];
-
-  //ANCHOR Cria um array de pokemons de acordo com a quantidade de repetições
-  for (let i = 1; i < 21; i++) {
-    await axios
-      .get(`${pokeURL}/${i}/`)
-      .then((response) => {
-        listPokemon[i - 1] = {
-          id: response.data.id,
-          name: response.data.name,
-          status: response.data.stats,
-          moves: response.data.moves,
-          types: response.data.types,
-          sprites: response.data.sprites,
-        };
-        //ANCHOR Se a largura estivar do tamanho que vc desejar,
-        //ele vai passar para o state
-        // listPokemon.length === 20
-        //   ? setPokemonList(listPokemon)
-        //   : alert("Erro no GET");
-      })
-      .catch((error) => {
-        alert("error.response");
-      });
-  }
+  const data = { states, setters, requests };
 
   return (
     <GlobalStateContext.Provider value={data}>
