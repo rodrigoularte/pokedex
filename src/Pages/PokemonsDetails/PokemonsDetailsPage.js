3e58e3c69e6766afec145c0175e12b6405dd3158
michael soto <michael_nickolas@hotmail.com>
2022-05-05 09:04:43 -0300

    Update PokemonsDetailsPage

--- src/Pages/PokemonsDetails/PokemonsDetailsPage.js
+++ src/Pages/PokemonsDetails/PokemonsDetailsPage.js
@@ -1,77 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Coluna1Container, Coluna2Container, DetailsContainer, PokeImg, PokeImgContainer, TipoContainer, AtaquesContainer } from "../PokemonsDetails/Styled"
-import { pokeURL } from '../../Constants/pokeURL'
-import Header from '../../Components/Header/Header'
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Coluna1Container,
+  Coluna2Container,
+  DetailsContainer,
+  PokeImg,
+  PokeImgContainer,
+  TipoContainer,
+  AtaquesContainer,
+} from "../PokemonsDetails/Styled";
+import { pokeURL } from "../../Constants/pokeURL";
+import Header from "../../Components/Header/Header";
 
 export const PokemonsDetails = () => {
-
-  const [pokemon, setPokemon] = useState([])
+  const [pokemon, setPokemon] = useState([]);
 
   const getPokemons = () => {
-
     axios
       .get(`${pokeURL}/pikachu`)
       .then((response) => {
-        setPokemon(response.data)
+        setPokemon(response.data);
       })
       .catch((error) => {
-        console.log(error.response)
-      })
-  }
+        console.log(error.response);
+      });
+  };
 
   useEffect(() => {
-    getPokemons()
-  }, [])
+    getPokemons();
+  }, []);
 
   return (
     <>
       <Header />
       {pokemon && pokemon.sprites && (
-      <DetailsContainer>
-        <PokeImgContainer>
-          <PokeImg src={pokemon.sprites.front_default} />
-          <PokeImg src={pokemon.sprites.back_default}/>
-        </PokeImgContainer>
+        <DetailsContainer>
+          <PokeImgContainer>
+            <PokeImg src={pokemon.sprites.front_default} />
+            <PokeImg src={pokemon.sprites.back_default} />
+          </PokeImgContainer>
 
-        <Coluna1Container>
-          <h2>Poderes</h2>
-          {pokemon && (
-            pokemon.stats.map((poder) => {
-              return(
-                <p key={poder.stat.name}>{poder.stat.name}: {poder.base_stat}</p>
-              )
-            })
-          )}
-        </Coluna1Container>
+          <Coluna1Container>
+            <h2>Poderes</h2>
+            {pokemon &&
+              pokemon.stats.map((poder) => {
+                return (
+                  <p key={poder.stat.name}>
+                    {poder.stat.name}: {poder.base_stat}
+                  </p>
+                );
+              })}
+          </Coluna1Container>
 
-        <Coluna2Container>
-          <TipoContainer>
-            {pokemon && pokemon.types.map((tipo) => {
-              return(
-                <p key={tipo.type.name}>{tipo.type.name}</p>
-              )
-            })}
-          </TipoContainer>
+          <Coluna2Container>
+            <TipoContainer>
+              {pokemon &&
+                pokemon.types.map((tipo) => {
+                  return <p key={tipo.type.name}>{tipo.type.name}</p>;
+                })}
+            </TipoContainer>
 
-          <AtaquesContainer>
-            <h2>Principais Ataques</h2>
-            {pokemon && pokemon.moves.map((ataque, i) => {
-              return(
-                i < 5 &&
-                <p key={ataque.move.name}>{ataque.move.name}</p>
-              )
-            })}
-          </AtaquesContainer>
-        </Coluna2Container>
-
-      </DetailsContainer>
+            <AtaquesContainer>
+              <h2>Principais Ataques</h2>
+              {pokemon &&
+                pokemon.moves.map((ataque, i) => {
+                  return (
+                    i < 5 && <p key={ataque.move.name}>{ataque.move.name}</p>
+                  );
+                })}
+            </AtaquesContainer>
+          </Coluna2Container>
+        </DetailsContainer>
       )}
     </>
-  )
-}
+  );
+};
 
-
-export default PokemonsDetails
+export default PokemonsDetails;
