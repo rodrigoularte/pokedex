b09344f90d0fcd0fe53084287cc9e0965e59eb7b
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-05 15:44:29 -0300

    pokedex7

--- src/Pages/Pokedex/PokedexPage.js
+++ src/Pages/Pokedex/PokedexPage.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import {listContainer} from "../Pokedex/Styled"
+import React from "react";
+import GlobalStateContext from "../../global/GlobalStateContext";
+import { useContext } from "react";
+import {PokeCard} from "Users/Luiz Maecelo/Desktop/projeto-pokedex/Shaw-pokedex7/src/Components/PokeCard/PokeCard"
+import { useNavigate } from "react-router-dom";
+import { goToPokeDetais } from "../../routes/coordinator";
+import { CardContainer } from "./styled";
 
 
 
-export const Pokedex = () => {
-   
-    
+
+const Pokedex = () => {
+  const { states } = useContext(GlobalStateContext);
+  const navigate = useNavigate();
+
+  const pokedexList =
+    states.pokedex &&
+    states.pokedex.sort((a, b) => {
+      return a.id - b.id
+    })
+      .map((pokemon, index) => {
         return (
-            <listContainer>
-                <h1>Pokedex Page</h1>
-            </listContainer>
+          <PokeCard
+            key={pokemon.name}
+            name={pokemon.name}
+            onClickCard={() => goToPokeDetais (navigate, pokemon.name)}
+            onClickCardBtnDetails={() => goToPokeDetais (navigate, pokemon.name)}
+            index={index}
+          />
         );
-    };
+      });
+  return (
+    <div>
+      <CardContainer>
+        {states.pokedex && pokedexList}
+        {states.pokedex.length === 0}
+      </CardContainer>
+    </div>
+  );
+};
+
+export default Pokedex;
 
 
-export default Pokedex
+
 
 
 
